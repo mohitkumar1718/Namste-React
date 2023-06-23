4b9ec3fc5e04c01825731b362081cb9886b65a28
MOHIT KUMAR <mohitkr7982@gmail.com>
2023-06-23 11:52:46 +0530

    react fragment

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom/client";
+
+ // react component
+ // class based component
+ // functional component
+
+// JSX heading
+const jsxHeading = <h1 className="heading" tabIndex="5">this is jsx heading</h1>;
+console.log(jsxHeading);
+ 
+// const heading = React.createElement("h1", { id: "heading" }, "this is react");
+// console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// functional component
+const Heading=()=>(
+
+    // React fragment -- behave like an empty tag
+    <React.Fragment>
+    <div>
+     <h1>this is functional component</h1>
+     </div>
+     <div>
+        <h1>this is second line</h1>
+     </div>
+     </React.Fragment>
+);
+root.render(<Heading/>);
