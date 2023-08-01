c7c65cec51b9b50efa2a878af34af56b9fe7723f
MOHIT KUMAR <mohitkr7982@gmail.com>
2023-08-01 23:27:29 +0530

    episode 6 mid

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,4 +2,4 @@ export const LOGO_URL=
 "https://img.freepik.com/premium-vector/restaurant-logo-design-template_79169-56.jpg?w=740";
 
 export  const CDN_URL=
-"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
