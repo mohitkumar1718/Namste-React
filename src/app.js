cdb8db51faf9a327d408feb78a9d2080bf410b8c
MOHIT KUMAR <mohitkr7982@gmail.com>
2023-08-28 23:43:49 +0530

    added RestaurantMenu components

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import { BrowserRouter,RouterProvider, createBrowserRouter,Outlet } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
 /**
  * -header
  *   -logo
@@ -26,13 +30,39 @@ import Body from "./components/Body";
    return(
       <div className="App">
          <Header/>
-         <Body/>
+         <Outlet/>
       </div>
    )
  }
 
 
 
+const AppRoute=createBrowserRouter([
+   {
+      path:"/",
+      element:<AppLayout/>,
+      errorElement:<Error/>,
+      children: [
+         {
+            path:"/",
+            element:<Body/>
+         },
+         {
+            path:"/about",
+            element:<About/>
+         },
+         {
+            path:"/contact",
+            element:<Contact/>
+         },
+         {
+            path:"/restaurant/:resId ",
+            element:<RestaurantMenu/>
+         }
+      ]
 
+   },
+   
+])
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(<RouterProvider router={AppRoute}/>);
