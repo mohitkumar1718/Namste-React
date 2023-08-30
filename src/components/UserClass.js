db58723bd6f3edd6b7c659542e5ebb326095baf5
MOHIT KUMAR <mohitkr7982@gmail.com>
2023-08-30 14:48:27 +0530

    class based component done

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,31 +4,43 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            count:0,
-            count2:2,
+            UserInfo:{
+                name:"Dummy Name",
+                location:"dummy Location",
+                avatar_url:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fpinnacle.works%2Fdummy-image%2F&psig=AOvVaw2LJUW3KK3G8ke0nMJEuxTT&ust=1693468959795000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCKCRo871g4EDFQAAAAAdAAAAABAE"
+
+            }
         };
-        console.log(this.props.name+ " constructor");
+                                                                    //   console.log(this.props.name+ " constructor");
     }
 
-    componentDidMount(){
-        console.log(this.props.name+ "Did mount");
+   async componentDidMount(){
+
+          const data= await fetch("https://api.github.com/users/mohitkumar1718");
+          const json=await data.json();
+          this.setState({
+            UserInfo:json,
+          }
+          )   
+          console.log(json);                                                       
     }
+    // it is called when the dom is updated or after the component Did Mount 
+   componentDidUpdate(){
+    console.log("updated Dom");
+   }
+   // it is called when the component is unmount or when we change the page or when we change the route
+   componentWillUnmount(){
+    console.log("component will unmount is called");
+   }
     render()
     {
-        console.log(this.props.name+ " render");
-
-        return(                      
+       const {name,location,avatar_url}=this.state.UserInfo;
+        return( 
+            // destructor on the fly
             <div className="userCard">
-               <h1>count: {this.state.count}</h1>
-               <button onClick={()=>{
-                this.setState({
-                    count:this.state.count+ 1,
-                })
-               }
-               }>Count Increase</button>
-               <h1>count2: {this.state.count2}</h1>
-               <h2>Name: {this.props.name}</h2>
-               <h3>Location : Faridabad</h3>
+               <img src={avatar_url} alt="non" />
+               <h2>Name: {name}</h2>
+               <h3>Location : {location}</h3>
                <h3>contact: @mohitkumar1718</h3>
                
                
@@ -37,3 +49,29 @@ class UserClass extends React.Component{
     }
 }
 export default UserClass;
+
+
+/***
+ * 
+ *-
+ ---- ---Mounting----------
+       - constructor is called with dummy data
+       - Render is called with dummy data
+         - HTML is loaded with dummy data
+       - component Did Mount is called
+         -API call
+         -this.setState  updated the state variable
+        
+----------update-------
+       - Render is called with API data
+       - HTML is loaded with updated data
+       - component Did update is called
+ */
+
+
+       /***
+        * in functional component we use return in useEffect just like componentDidUNmount it behaves the same for functional component 
+        * 
+        * 
+        * 
+        */
