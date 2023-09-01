7400434d642e59e42673c3e7585b966df0cc0309
MOHIT KUMAR <mohitkr7982@gmail.com>
2023-09-01 23:56:37 +0530

    applied tailwind & added a higher order component

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -7,17 +7,40 @@ const RestaurantCard=(props)=>{
    const cuisines=resData?.info?.cuisines;
    const avgRating=resData?.info?.avgRating;
    const deliveryTime=resData?.info?.sla?.deliveryTime;
-
+   const costForTwo=resData?.info?.costForTwo;
+   let cuisine=[];
+   if(cuisines.length>3){
+    cuisine.push(cuisines[0]);
+    cuisine.push(cuisines[1]);
+    cuisine.push(cuisines[2]);
+   }
+   else cuisine=cuisines;
+   
  
     return(
-      <div className="res-card">
-         <img  className="res-logo" src={CDN_URL+cloudinaryImageId}  alt="img" />
-         
-         <h3>{name}</h3>
-         <h4>{cuisines.join(", ")}</h4>
-         <h4>{avgRating}⭐</h4>
+      <div className="m-4  p-4 w-[270px] h-[400px] bg-gray-100 hover:bg-green-100 hover:shadow-xl rounded-lg cursor-pointer  ">
+         <img  className="res-logo  rounded-lg  h-[200px] w-[240px]" src={CDN_URL+cloudinaryImageId}  alt="img" />
+  
+         <h4 className=" font-bold py-2 text-lg">{name}</h4>
+         <h4 className=" from-neutral-500">{ cuisine.join(", ")}</h4>
+         <h4 >Rating:{avgRating}⭐</h4>
          <h4>{deliveryTime} minutes </h4>
+         <h4>{costForTwo}</h4>
       </div>
     )
   }
+  
+  export  const RestaurantLabel=(RestaurantCard)=>{
+    return (props)=>{
+     return(
+      <div>
+        <label className=" absolute bg-green-400 colo text-white mx-4 p-2  rounded-lg justify-start">Promoted</label>;
+        <RestaurantCard  {...props}/>
+      </div>  
+     
+      
+     )
+    }
+  }
+  
   export default RestaurantCard;
